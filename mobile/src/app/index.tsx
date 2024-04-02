b1f820a55682565d5482e8c559906a9b91ee7db5
vandodev <vandoaparecido@hotmail.com>
2024-04-01 21:35:43 -0300

    :lock: :closed_lock_with_key: :key: Mobile - Iniciando na estilização

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,9 +1,22 @@
-import {View, Text} from "react-native"
+import {View, Text, StyleSheet} from "react-native"
 
 export default function Home(){
     return(
-        <View>
-            <Text>Olá mundo</Text>
+        // <View style={{backgroundColor:"red", flex: 1}}>
+        <View style={styles.container}>
+            <Text style={styles.title}>Olá mundo</Text>
         </View>
     )
 }
+ const styles = StyleSheet.create({
+    container:{
+        backgroundColor:"#000",
+        flex: 1, 
+        justifyContent:"center",
+        alignItems:"center"
+    },
+    title:{
+        fontSize:24,
+        color:"white"
+    }
+ })
