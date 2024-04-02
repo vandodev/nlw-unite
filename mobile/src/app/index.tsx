64965db2151ce4fef6dbf99651cd99c43198d8f8
vandodev <vandoaparecido@hotmail.com>
2024-04-01 21:51:23 -0300

    :lock: :closed_lock_with_key: :key: Mobile - Configurando o tailwind e alterando tema padrão

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,22 +1,9 @@
-import {View, Text, StyleSheet} from "react-native"
+import {View, Text} from "react-native"
 
 export default function Home(){
-    return(
-        // <View style={{backgroundColor:"red", flex: 1}}>
-        <View style={styles.container}>
-            <Text style={styles.title}>Olá mundo</Text>
+    return(        
+        <View className="flex-1 bg-green-500 items-center justify-center p-8">
+            <Text className="text-white">Olá mundo</Text>
         </View>
     )
 }
- const styles = StyleSheet.create({
-    container:{
-        backgroundColor:"#000",
-        flex: 1, 
-        justifyContent:"center",
-        alignItems:"center"
-    },
-    title:{
-        fontSize:24,
-        color:"white"
-    }
- })
