53012c75a7d0a0440a09e2427ba1694f0de93cac
vandodev <vandoaparecido@hotmail.com>
2024-04-03 07:11:54 -0300

    :lock: :closed_lock_with_key: :key: Mobile - Compentente Header

--- mobile/src/app/ticket.tsx
+++ mobile/src/app/ticket.tsx
@@ -1,18 +1,11 @@
-import {
-    Text,
-    View,
-    Alert,
-    Modal,
-    Share,
-    StatusBar,
-    ScrollView,
-    TouchableOpacity,
-  } from "react-native"
+import { Header } from "@/components/header"
+import {View,StatusBar,} from "react-native"
 
 export default function Ticket() {
     return(
         <View className="flex-1 bg-green-500">
             <StatusBar barStyle="light-content" />
+            <Header title="Minha credencial" />
         </View>
     )
 }
