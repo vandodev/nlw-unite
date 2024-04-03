2f7e61916bcd8e9aedfd4e33079ea06f42dced0a
vandodev <vandoaparecido@hotmail.com>
2024-04-03 07:50:36 -0300

    :lock: :closed_lock_with_key: :key: Mobile - Componente de credenciais

--- mobile/src/app/ticket.tsx
+++ mobile/src/app/ticket.tsx
@@ -1,3 +1,4 @@
+import { Credential } from "@/components/credential"
 import { Header } from "@/components/header"
 import {View,StatusBar,} from "react-native"
 
@@ -6,6 +7,7 @@ export default function Ticket() {
         <View className="flex-1 bg-green-500">
             <StatusBar barStyle="light-content" />
             <Header title="Minha credencial" />
+            <Credential />
         </View>
     )
 }
---
+++ mobile/src/components/credential.tsx
@@ -0,0 +1,41 @@
+import { View, Image,Text, ImageBackground, TouchableOpacity } from "react-native";
+
+export function Credential(){
+    return(
+        <View className="w-full self-stretch items-center">
+            <Image
+                source={require("@/assets/ticket/band.png")}
+                className="w-24 h-52 z-10"
+            />
+
+            <View className="bg-black/20 self-stretch items-center pb-6 border border-white/10 mx-3 rounded-2xl -mt-5">
+                <ImageBackground
+                    source={require("@/assets/ticket/header.png")}
+                    className="px-6 py-8 h-40 items-center self-stretch border-b border-white/10 overflow-hidden"
+                    >
+                    <View className="w-full flex-row items-center justify-between">
+                        <Text className="text-zinc-50 text-sm font-bold">
+                         Unite Submit
+                        </Text>
+                        <Text className="text-zinc-50 text-sm font-bold">#123</Text>
+                    </View>
+
+                    <View className="w-40 h-40 bg-black rounded-full" />
+                </ImageBackground>
+
+                <Image source={{uri: "https://github.com/vandodev.png"} } className="w-36 h-36  rounded-full -mt-20" />
+                <Text className="font-bold text-2xl text-zinc-50 mt-4">Evandro oliveira</Text>
+                <Text className="font-regular text-base text-zinc-300 mb-4">vandoaparecido@hotmail.com</Text>
+
+                <Image
+                    source={require("@/assets/ticket/qrcode.png")}
+                    className="w-32 h-32"
+                />
+
+                <TouchableOpacity activeOpacity={0.7} className="mt-6">
+                    <Text className="font-body text-orange-500 text-sm">Ampliar qrcode</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
