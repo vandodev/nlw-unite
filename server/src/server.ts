e84f4cd96932f9f1d818c4e35f7692630c503b8d
vandodev <vandoaparecido@hotmail.com>
2024-04-04 08:41:19 -0300

    :lock: :closed_lock_with_key: :key: Server - Cadastrando evento

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,32 @@
 import fastify from "fastify"
+import { z } from "zod"
+import {PrismaClient} from "prisma/prisma-client"
 
 const app = fastify()
 
-app.get('/', () =>{
-    return "hello nlw"
+const prisma = new PrismaClient({
+    log: ['query'],
+})
+
+app.post('/events',async (request, reply) =>{
+    // console.log(request.body)
+    const createEventSchema = z.object({
+        title: z.string().min(4),
+        details: z.string().nullable(),
+        maximumAttendees: z.number().int().positive().nullable()
+    })
+
+    const data = createEventSchema.parse(request.body)
+
+  const event =  await prisma.event.create({
+        data: {
+            title: data.title,
+            details: data.details,
+            maximumAttendees: data.maximumAttendees,
+            slug: new Date().toString()
+        },
+    })
+    return {eventId: event.id}
 })
 
 app.listen({port: 3333}).then(() =>{
