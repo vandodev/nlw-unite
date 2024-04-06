083b1d9dbf49085e4d8f56fbf3c7e56dfce79ebf
vandodev <vandoaparecido@hotmail.com>
2024-04-06 08:50:50 -0300

    :lock: :closed_lock_with_key: :key: Server - Buscando dados de um evento específico

---
+++ server/src/routes/get-event.ts
@@ -0,0 +1,31 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+
+export async function getEvent(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .get('/events/:eventId', {
+      schema: {
+        params: z.object({ 
+          eventId: z.string().uuid(),
+        }),
+        response: { },
+      }
+    }, async (request, reply) => {
+      const { eventId } = request.params
+
+      const event = await prisma.event.findUnique({
+        where: {
+          id: eventId,
+        }
+      })
+
+      if (event === null) {
+        throw new Error('Event not found.')
+      }
+
+      return reply.send({ event})
+    })
+}
