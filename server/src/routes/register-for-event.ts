aca6fdb9240e5bb2d2d2fd1f514fefed809bbf79
vandodev <vandoaparecido@hotmail.com>
2024-04-06 08:22:09 -0300

    :lock: :closed_lock_with_key: :key: Server - Limitando números de participantes por evento

--- server/src/routes/register-for-event.ts
+++ server/src/routes/register-for-event.ts
@@ -38,6 +38,21 @@ export async function registerForEvent(app: FastifyInstance) {
         throw new Error('This e-mail is already registered for this event.')
       }
 
+      const event = await prisma.event.findUnique({
+        where:{
+          id: eventId
+        }
+      })
+
+      const amountOfAttendeesForEvent = await prisma.attendee.count({
+        where: {
+          eventId,
+        }
+      })
+
+      if (event?.maximumAttendees && amountOfAttendeesForEvent >= event.maximumAttendees) {
+        throw new Error('The maximum number of attendees for this event has been reached.')
+      }
 
       const attendee = await prisma.attendee.create({
         data: {
