f14e3f2a3162d1514528d3f4e4e61f302b8d3201
vandodev <vandoaparecido@hotmail.com>
2024-04-07 07:41:45 -0300

    :lock: :closed_lock_with_key: :key: Server - Criando checkInURL

--- server/src/routes/get-attendee-badge.ts
+++ server/src/routes/get-attendee-badge.ts
@@ -16,7 +16,7 @@ export async function getAttendeeBadge(app: FastifyInstance) {
             badge: z.object({
               name: z.string(),
               email: z.string().email(),
-              eventTitle: z.string(),
+              eventTitle: z.string(), 
               checkInURL: z.string().url(),
             })
           })
@@ -45,9 +45,8 @@ export async function getAttendeeBadge(app: FastifyInstance) {
       }
 
       const baseURL = `${request.protocol}://${request.hostname}`
-
       const checkInURL = new URL(`/attendees/${attendeeId}/check-in`, baseURL)
-
+     
       return reply.send({
         badge: {
           name: attendee.name,
