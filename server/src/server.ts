c8f5f3414eba5ff3c821c39dc0994cde1dde5df7
vandodev <vandoaparecido@hotmail.com>
2024-04-08 08:55:08 -0300

    :lock: :closed_lock_with_key: :key: Server - Configurando Cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,8 +9,14 @@ import { getAttendeeBadge } from "./routes/get-attendee-badge";
 import { checkIn } from "./routes/check-in";
 import { getEventAttendees } from "./routes/get-event-attendees";
 import { errorHandler } from "./error-handler";
+import fastifyCors from "@fastify/cors";
+
 
 const app = fastify()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
 
 app.register(fastifySwagger, {
     swagger: {
@@ -41,6 +47,6 @@ app.register(getEventAttendees)
 
 app.setErrorHandler(errorHandler)
 
-app.listen({port: 3333}).then(() =>{
+app.listen({port: 3333, host: "0.0.0.0"}).then(() =>{
     console.log("http server running")
 })
