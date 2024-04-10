aad6662d736773cdd892aec8cc6ad997fbe5162f
vandodev <vandoaparecido@hotmail.com>
2024-04-10 07:19:10 -0300

    :lock: :closed_lock_with_key: :key: Mobile - Animando cracha com Moti e reanimated

--- mobile/src/components/credential.tsx
+++ mobile/src/components/credential.tsx
@@ -1,8 +1,9 @@
-import { View, Image,Text, ImageBackground, TouchableOpacity } from "react-native";
+import { View, Image,Text, ImageBackground, TouchableOpacity, useWindowDimensions } from "react-native";
 import { Feather } from "@expo/vector-icons"
 import { colors } from "@/styles/colors"
 import { QRCode } from "./qrcode";
 import { BadgeStore } from "@/store/badge-store"
+import { MotiView } from "moti"
 
 type Props = {
     data: BadgeStore
@@ -12,8 +13,33 @@ type Props = {
   }
 
 export function Credential({onChangeAvatar,data, onExpandQRCode }: Props){
+    const { height } = useWindowDimensions()
     return(
-        <View className="w-full self-stretch items-center">
+        <MotiView
+         className="w-full self-stretch items-center"
+         from={{
+            opacity: 0,
+            translateY: -height,
+            rotateZ: "50deg",
+            rotateY: "30deg",
+            rotateX: "30deg",
+          }}
+          animate={{
+            opacity: 1,
+            translateY: 0,
+            rotateZ: "0deg",
+            rotateY: "0deg",
+            rotateX: "0deg",
+          }}
+          transition={{
+            type: "spring",
+            damping: 20,
+            rotateZ: {
+              damping: 15,
+              mass: 3,
+            },
+          }}
+         >
             <Image
                 source={require("@/assets/ticket/band.png")}
                 className="w-24 h-52 z-10"
@@ -58,6 +84,6 @@ export function Credential({onChangeAvatar,data, onExpandQRCode }: Props){
                     <Text className="font-body text-orange-500 text-sm">Ampliar qrcode</Text>
                 </TouchableOpacity>
             </View>
-        </View>
+        </MotiView>
     )
 }
