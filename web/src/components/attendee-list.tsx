6346d21a0f964f3a04792e8e27c3b0f7efc81505
vandodev <vandoaparecido@hotmail.com>
2024-04-11 07:41:01 -0300

    :lock: :closed_lock_with_key: :key: Web - Titulo e input

---
+++ web/src/components/attendee-list.tsx
@@ -0,0 +1,18 @@
+import {Search} from 'lucide-react';
+  
+  export function AttendeeList() {
+    return (
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-3 items-center">
+          <h1 className="text-2xl font-bold">Participantes</h1>
+          <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg flex items-center gap-3">
+            <Search className="size-4 text-emerald-300" />
+            <input
+              className="bg-transparent flex-1 outline-none border-0 p-0 text-sm"
+              placeholder="Buscar participante..."
+            />
+          </div>
+        </div>        
+      </div>
+    );
+  }
